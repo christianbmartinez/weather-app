90282a30a16270aea071f1576c1329cc91144087
Christian Martinez <christian@mixdlabs.com>
2023-05-21 17:50:00 -0600

    Added descriptive comments, used ternary operator

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Store our ID's into variables
 const search = document.getElementById('search')
 const form = document.querySelector('form')
 const recentSearches = document.getElementById('recent-searches')
@@ -29,44 +30,39 @@ form.onsubmit = async (e) => {
     `http://api.openweathermap.org/data/2.5/forecast?q=${search.value}&units=imperial&appid=486060bafc6b2d129e48e59a2e9520a8`
   )
   const data = await response.json()
+  // If the error code is anything but 200 (success), lets throw an error as an alert
   if (data.cod !== '200') {
+    // Clear the input form
     search.value = ''
     alert(`Error:${data.cod} That city wasn't found. Please try again!`)
   } else {
+    // Otherwise, lets first set the city for the forecast. This can also be done using search.value, but it's lowercase
     forecastCity.textContent = `5 Day forecast for ${data.city.name}`
-    console.log(data)
+    // Map over all of the data in the array
     data.list.map((weather) => {
-      console.log(weather)
       // We have a lot of data in the array for the same days.
       // I assumed its for morning, day, mid day, night, midnight.
-      // In military time, if it's around mid day, lets return that weather data
-      if (weather.dt_txt.includes('15:00:00')) {
-        forecast.innerHTML += `
+      // In military time, if it's around mid day, lets return that weather forecast data
+      // We can assign the data using a template literal to dynamically inject the cards and
+      // data to the page. It might no be best practice, but it works great and gets students
+      // prepared with react. (I have been using react for a few years)
+      weather.dt_txt.includes('15:00:00')
+        ? (forecast.innerHTML += `
       <div class="col p-3">    
       <div class="row">
           <div class="card" style="width: auto; margin-right:1em;">
               <div class="card-body">
-                  <h5 id="forecast-date">${dayjs(weather.dt_txt).format(
-                    'M/D/YYYY'
-                  )}</h5>
-                  <img class="card-img-top my-2" src="/img/icons8-summer-48.png"  style='width: 25px; height: 25px;' alt="Card image cap">
-                <p id="forecast-temp" class="card-text">Temp: ${
-                  weather.main.temp
-                }°F</p>
-                <p id="forecast-wind" class="card-text">Wind: ${
-                  weather.wind.speed
-                }mph</p>
-                <p id="forecast-humidity" class="card-text">Humidity: ${
-                  weather.main.humidity
-                }%</p>
+                  <h5>${dayjs(weather.dt_txt).format('M/D/YYYY')}</h5>
+                  <img class="card-img-top my-2" src="/img/icons8-summer-48.png" style='width: 25px; height: 25px;' alt="Card image">
+                <p class="card-text">Temp: ${weather.main.temp}°F</p>
+                <p class="card-text">Wind: ${weather.wind.speed}mph</p>
+                <p class="card-text">Humidity: ${weather.main.humidity}%</p>
               </div>
           </div>                              
       </div>
   </div>
-      `
-      } else {
-        return
-      }
+      `)
+        : null
     })
   }
 }
